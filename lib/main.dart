7cf60507f1e773314e205a3fbf325493f6d367ac
Abdulrahman Alaa Eldin  <abdulrahmanalaa@hotmail.com>
2020-06-27 03:10:23 +0400

    intial page scoffelding

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
-//arrow syntax 
+//arrow syntax
 void main(List<String> args) => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello world'));
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('First app bar!'),
+      ),
+      body: Text('some dummy text '),
+    ));
+    //return MaterialApp(home: Text('Hello world'));
   }
 }
 
-
 //pubspec.yml contain all imported packages
-//class feature in constructor as in TS 
+//class feature in constructor as in TS
 class Test {
   String name;
   Test({this.name});
