70469834aebf25e6c9c2bf59717527851f2dff00
Abdulrahman Alaa Eldin  <abdulrahmanalaa@hotmail.com>
2020-06-27 03:03:52 +0400

    syntax arrow and named constructor in dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
-}
+//arrow syntax 
+void main(List<String> args) => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Text('Hello world'));
   }
+}
+
+
+//pubspec.yml contain all imported packages
+//class feature in constructor as in TS 
+class Test {
+  String name;
+  Test({this.name});
 }
